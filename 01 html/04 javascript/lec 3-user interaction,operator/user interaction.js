0c9d7f09b7f63b6d9008701c79ba6d2fd340f3e5
arpitapriyadarshini12112004-spec <arpitapriyadarshini12112004@gmail.com>
2026-07-21 09:34:40 +0530

    lec 3

---
+++ 01 html/04 javascript/lec 3-user interaction,operator/user interaction.js
@@ -0,0 +1,19 @@
+//alert("Hello Everyone!")
+//window.alert("Good Morning")
+
+// let name = prompt("Enter your name:-")
+ //console.log(name)
+
+ //if(name){
+   // console.log("your name is",name)
+//}else{
+    //console.log("Re enter user name")
+    //console.log("name is", prompt("re enter username"))
+//}
+
+//let choice = confirm("Are you enjoying js classes ?")
+//console.log(choice)
+
+console.log("Hii")      //normal
+console.warn("Hello")     //yellow
+console.error("bye")      //red
