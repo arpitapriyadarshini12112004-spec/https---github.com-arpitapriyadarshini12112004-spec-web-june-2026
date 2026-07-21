0dc7a45758147a9164e043ce47d018feb8ae76bf
arpitapriyadarshini12112004-spec <arpitapriyadarshini12112004@gmail.com>
2026-07-21 09:33:47 +0530

    lec 1

---
+++ 01 html/04 javascript/lec 1-intro to js,keywords,datatypes,variables/diff betn var,let and const.js
@@ -0,0 +1,52 @@
+//function myfun(){
+  // let var1 = 10
+ //  const var2 = 20
+   //var var3 = 30
+ 
+//}
+
+//myfun()
+
+//console.log(var1)
+//console.log(var2)
+//console.log(var3)
+
+// ========== DT - 20 7 26
+//(i){
+  //  let a = 10;
+   // const b = 20;
+  //  var c = 30
+//}
+
+//function fun(){
+   // debugger
+  //  let a1 = 10;
+   // const b1 = 20;
+   // var c1 = 30
+
+   // console.log("hiiiiiii")
+//}
+
+//fun()
+
+//(ii)
+//const name = "Avinash"
+//console.log("Before reassign",name)
+//name = "Majhi"
+//console.log("After reassign",name)
+
+//(iii)
+//let instituteName = "JT"
+//console.log("instituteName------", instituteName)
+//let instituteName = "Java Technocrat"
+//console.log("instituteName=======", instituteName)
+
+//(iv)
+debugger
+console.log(x)
+console.log(y)
+
+
+var x = 10
+let y = 20;
+const z = 30
