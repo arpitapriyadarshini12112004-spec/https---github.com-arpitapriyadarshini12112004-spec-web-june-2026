9a7a9edacb0f7d4396b14cb0bed1acdefd7d0d48
arpitapriyadarshini12112004-spec <arpitapriyadarshini12112004@gmail.com>
2026-09-07 20:43:39 +0530

    hello-react

---
+++ 01 html/05 React/hello-react/src/Arpita.jsx
@@ -0,0 +1,6 @@
+function Arpita() {
+    return(
+        <h3>How it's going</h3>
+    )
+}
+export default Arpita
---
+++ 01 html/05 React/hello-react/src/Product.jsx
@@ -0,0 +1,18 @@
+// export default function Product(props) {
+    // console.log("....", props);
+//   const name = props.name    
+//   const price = props.price  
+//   const { name, price } = props
+
+
+  export default function Product({name, price, ...others}) {  
+return (
+    <>
+    <h3> Name:- {name}</h3>
+    <p>Price:- {price}</p>
+    <p>{others.description ? `description:- ${others.description}` : ''}</p>
+    <p>{others.brand ? `brand:- ${others.brand}` : ''}</p>
+    </>
+    )
+   }
+   
